0ccbb7edda904257974932f55548700d8d68852b
Alfonso Graziano <alfonso@cofenster.com>
2023-03-28 16:14:24 +0200

    Rrtrocompatibility testing

--- src/main/java/dev/inspector/agent/App.java
+++ src/main/java/dev/inspector/agent/App.java
@@ -1,7 +1,9 @@
 package dev.inspector.agent;
 
 import dev.inspector.agent.model.*;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import java.security.Security;
 import java.util.Date;
 
 /**
@@ -13,9 +15,13 @@ public class App
     public static void main( String[] args )
     {
 
+        Security.addProvider(new BouncyCastleProvider());
+
+
         Config config = new Config("81e6d4df93e1bfad8e9f3c062022e3a0d8a77dce");
         Inspector inspector = new Inspector(config);
         inspector.startTransaction("Test Java segment 1");
+        /*
         Segment segmentRef =  inspector.addSegment((segment) -> {
             int millisecToWait = 3000;
             long startingTime = new Date().getTime();
@@ -24,6 +30,8 @@ public class App
             }
             return segment;
         }, "test async", "test label", false);
+        */
+
 
         //TODO: Check if we can implement an auto flush
         //See the node env
